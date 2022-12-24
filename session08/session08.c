2b9e19d13193cfaffdb0e9365212c0ce9cf0183a
unknown <waqarzakaiot@gmail.com>
2022-12-24 20:38:10 +0500

    Different Datatypes in C Language

---
+++ session08/session08.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main(void){
+	int age;  //Declaration
+	age = 10; // Initialization or Assignment
+	int age01 = 10; //Declaration + Initialization
+	printf("This is the first variable: %d",age);
+	
+}
