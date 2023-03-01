5d7564b9a41384f5d48cacb9fd2a8e88387adfdd
unknown <waqarzakaiot@gmail.com>
2023-03-01 10:14:31 +0500

    Header Files in C Language

---
+++ session38/math_function.h
@@ -0,0 +1,15 @@
+#ifndef MATH_FUNCTION_H
+#define MATH_FUNCTION_H
+
+
+int add_number(int x, int y){
+    int result = x +y;
+    return result;
+}
+int mul_number(int x, int y){
+    int result = x*y;
+    return result;
+}
+
+
+#endif
---
+++ session38/session38.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<conio.h>
+#include "math_function.h"
+void main(){
+    int a = 10, b = 20;
+    printf("The sum of two variable is %d\n",add_number(a,b));
+    printf("The multiplication of two variable is %d\n",mul_number(a,b));
+}
